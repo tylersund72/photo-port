4e4c88187423ba5a759ec5087c939af30ecb44f5
Tyler <tylersundquist3@gmail.com>
2022-01-04 18:09:48 -0700

    tests passed

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,8 +3,6 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import About from "..";
 
-const { asFragment } = render(<About />);
-
 afterEach(cleanup);
 
 describe("About component", () => {
@@ -16,7 +14,9 @@ describe("About component", () => {
   //second test
   it("matches snapshot DOM note structure", () => {
     // render About
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
 
-expect(asFragment()).toMatchSnapshot();
